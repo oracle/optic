05e547f94ba18243088eb7b623567bbb41f79181
Seth Lippman <seth.lippman@oracle.com>
2025-07-25 14:33:17 -0600

    feat: 🥅 add retry logic to OpenSearch API calls
    fix: 🩹 moved API call notification messages to correctly report order of actions

--- src/optic/common/api.py
+++ src/optic/common/api.py
@@ -4,23 +4,55 @@
 # ** Licensed under the Universal Permissive License v 1.0
 # ** as shown at https://oss.oracle.com/licenses/upl/
 
+import logging
+
 import requests
 import urllib3
+from requests.adapters import HTTPAdapter
 from requests.auth import HTTPBasicAuth
+from urllib3.util.retry import Retry
 
 from optic.common.exceptions import OpticAPIError
+
+logging.basicConfig(level=logging.INFO)
 
 urllib3.disable_warnings()
 
 
 class OpenSearchAction:
-    def __init__(self, base_url="", usr="", pwd=None, verify_ssl=True, query=""):
+    def __init__(
+        self,
+        base_url="",
+        query="",
+        retries=3,
+        backoff_factor=2,
+        status_forcelist=(500, 502, 503, 504),
+        usr="",
+        pwd=None,
+        verify_ssl=True,
+    ):
+        """
+        Wraps the methods required to execute REST calls against the OpenSearch API.
+        Args:
+            base_url (str): The URL to send the request to
+            query (str): additional string added to the end of base_url
+            retries (int): Maximum number of retries
+            backoff_factor (float): Backoff factor to apply between retries in seconds
+            status_forcelist (tuple): HTTP status codes to retry on
+            usr (str): username to be used in BasicAuth header
+            pwd (str): password to be used in BasicAuth header
+            verify_ssl (boolean): set SSL verification mode
+
+        """
+
         self.base_url = base_url
+        self.query = query
+        self.retries = retries
+        self.backoff_factor = backoff_factor
+        self.status_forcelist = status_forcelist
         self.usr = usr
         self.pwd = pwd
         self.verify_ssl = verify_ssl
-        self.query = query
-
         self._response = None
 
     @property
@@ -31,18 +63,35 @@ class OpenSearchAction:
         :return: JSON-like object with response data
         :rtype: list | dict
         """
+
         if not self._response:
+            url = self.base_url + self.query
+            logging.debug(
+                f"creating REST request to {url} with "
+                f"{self.retries} retries, backoff {self.backoff_factor}, and ssl verify {self.verify_ssl}"
+            )
+            basic_auth = HTTPBasicAuth(self.usr, self.pwd)
+            retry_strategy = Retry(
+                total=self.retries,
+                backoff_factor=self.backoff_factor,
+                status_forcelist=self.status_forcelist,
+            )
+            session = requests.Session()
+            session.mount("https://", HTTPAdapter(max_retries=retry_strategy))
+            session.mount("http://", HTTPAdapter(max_retries=retry_strategy))
+
             try:
-                basic = HTTPBasicAuth(self.usr, self.pwd)
-                self._response = requests.get(
-                    self.base_url + self.query,
+                self._response = session.get(
+                    url,
                     verify=self.verify_ssl,
-                    auth=basic,
+                    auth=basic_auth,
                     timeout=6,
                 )
                 self._response.raise_for_status()
             except requests.exceptions.RequestException as err:
-                raise OpticAPIError(str(err)) from err
+                raise OpticAPIError(
+                    f"Request failed after {self.retries} retries {str(err)}"
+                ) from err
 
             self._response = self._response.json()
 
